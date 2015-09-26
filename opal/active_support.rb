346ed3944a3c175f70e3c74738845fa9159d5e4c
Brady Wied <brady@bswtechconsulting.com>
2015-09-26 17:18:30 -0600

    Got past phone helper errors

--- opal/active_support.rb
+++ opal/active_support.rb
@@ -1,3 +1,5 @@
+# TODO: Segment these out into different requires
+
 require 'active_support/core_ext/module/delegation'
 
 class Module
@@ -177,4 +179,31 @@ require 'active_support/tagged_logging'
 
 # avoid logger check in testing/lagged_logging
 module Rails
+end
+
+require 'active_support/number_helper/number_converter'
+require 'active_support/number_helper/number_to_phone_converter'
+
+class ActiveSupport::NumberHelper::NumberToPhoneConverter
+  def convert
+    str = country_code(opts[:country_code])
+    str += convert_to_phone_number(number.to_s.strip)
+    str + phone_ext(opts[:extension])
+  end
+
+  def convert_with_area_code(number)
+    # string mutate
+    #number.gsub!(/(\d{1,3})(\d{3})(\d{4}$)/, "(\\1) \\2#{delimiter}\\3")
+    #number
+    number.gsub(/(\d{1,3})(\d{3})(\d{4}$)/, "(\\1) \\2#{delimiter}\\3")
+  end
+
+  def convert_without_area_code(number)
+    # string mutate
+    # number.gsub!(/(\d{0,3})(\d{3})(\d{4})$/, "\\1#{delimiter}\\2#{delimiter}\\3")
+    # number.slice!(0, 1) if start_with_delimiter?(number)
+    # number
+    subbed = number.gsub(/(\d{0,3})(\d{3})(\d{4})$/, "\\1#{delimiter}\\2#{delimiter}\\3")
+    start_with_delimiter?(number) ? subbed.slice(0, 1) : subbed
+  end
 end
